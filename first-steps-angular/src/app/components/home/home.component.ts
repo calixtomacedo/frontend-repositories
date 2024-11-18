79525d9fdd702e0a3e2a324802d1270a8e42a172
Calixto Rodrigues Macedo <calixto.macedo@gmail.com>
2024-11-18 08:14:21 -0300

    Sinal, Diretivas, e mais.

---
+++ first-steps-angular/src/app/components/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, signal } from '@angular/core';
+import { FooterModule } from '../footer/footer.module';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FooterModule, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  name = signal("Calixto Macedo");
+  bikeUrl = "https://media.istockphoto.com/id/1402134774/photo/professional-road-cyclist-on-a-training-ride.jpg?s=612x612&w=0&k=20&c=CB2o_DXMgH15MLa1CEqWwZVtVb3rpRgejV3UFnUwF_U=" 
+  count = 0;
+  contador = signal(0);
+  boolean = false;
+  list = ["Calixto", "Rosy", "Lucca", "Thamyres"];
+
+  myFunction() {
+    console.log(this.name);
+    this.contador.update(value => value + 1);
+  }
+
+  printContent() {
+    this.name.set(this.name() +" - "+ this.count++);
+    console.log(this.name());
+  }
+
+}
