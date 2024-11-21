7a0ae6b3260783b7aefa4157f7da52d9c6ab3513
Calixto Rodrigues Macedo <calixto.macedo@gmail.com>
2024-11-20 21:22:06 -0300

    Usando Roteamento com Lazy Loading

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses/courses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  { path: 'courses', component: CoursesComponent }
+];
